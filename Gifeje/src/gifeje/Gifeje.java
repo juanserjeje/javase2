c0a0fecc13b83f2bf97c4f65ee9bc048eb497227
giogonza21 <giovannygonzalez806@gmail.com>
2024-05-10 12:45:51 -0500

    hola linde

---
+++ Gifeje/src/gifeje/Gifeje.java
@@ -0,0 +1,13 @@
+package gifeje;
+
+public class Gifeje {
+
+    
+    
+    public static void main(String[] args) {
+            System.out.println("oliii tu");
+            System.out.println("chaooo tu\n"
+            +"ya me voy tao <3");
+    }
+    
+}
