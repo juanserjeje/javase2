3a89bd03fd87425cec3feaaa322402a2b9bff8bf
juanserjeje <juane.serranov@konradlorenz.edu.co>
2024-05-10 13:02:10 -0500

    Update Gifeje.java

--- Gifeje/src/gifeje/Gifeje.java
+++ Gifeje/src/gifeje/Gifeje.java
@@ -7,7 +7,7 @@ public class Gifeje {
     public static void main(String[] args) {
             System.out.println("oliii tu");
             System.out.println("chaooo tu\n"
-            +"ya me voy tao <3");
+            +"ya me voy tao <3\n"+"sapo mano");
     }
     
 }
